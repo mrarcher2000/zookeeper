52367b57f979d772df3bdc81a76142929ceaf594
unknown <archernich@gmail.com>
2021-04-06 19:44:19 -0700

    add GET findById

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ function filterByQuery(query, animalsArray) {
 };
 
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -63,6 +69,16 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 
